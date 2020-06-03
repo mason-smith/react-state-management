3b4507f321b21f23fcb481de54d6f6fff26ac50b
Mason <mason.smith@i4-insight.com>
2020-06-03 15:07:59 -0600

    Starting new project structure

--- redux-vanilla/src/actions/visibilityActions.ts
+++
@@ -1,5 +0,0 @@
-import { SET_VISIBILITY_FILTER } from './actionTypes';
-
-export function setVisibilityFilter(filter: string) {
-  return { type: SET_VISIBILITY_FILTER, filter };
-}
--- redux-vanilla/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-
-// Local Dependencies
-import reducer from '../reducers/rootReducer';
-
-const store = createStore(reducer);
-
-export default store;
